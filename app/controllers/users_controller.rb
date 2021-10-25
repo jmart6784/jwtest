423debef280681b39ecd2d0908f73f207ce9a9a3
jmart6784 <jmartinez6784@yahoo.com>
2021-10-24 23:59:36 -0700

    Added login action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,17 @@ class UsersController < ApplicationController
     end
   end
 
+  def login
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
+  end
+
   private
 
   def user_params
