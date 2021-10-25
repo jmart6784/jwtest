4dad02082401b3e047b1c3e734de0825537df2f7
jmart6784 <jmartinez6784@yahoo.com>
2021-10-24 23:53:09 -0700

    Added user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token}
+    else
+      render json: {error: "Invalid username or password"}
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :password, :age)
+  end
 end
