c453603d8546a59a36d79d4b4773dc127063413f
jmart6784 <jmartinez6784@yahoo.com>
2021-10-28 00:34:36 -0700

    Fixed user show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :notes
-  resource :users, only: [:create, :show]
+  resource :users, only: [:create]
+  get "users/:id", to: "users#show"
   post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
   get "/present_user", to: "users#present_user"
