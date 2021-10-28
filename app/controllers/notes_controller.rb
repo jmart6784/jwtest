6e859ce754689c6f780fe62b7926784f1aa9c82c
jmart6784 <jmartinez6784@yahoo.com>
2021-10-27 23:58:59 -0700

    fixed typo

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -38,7 +38,7 @@ class NotesController < ApplicationController
   # DELETE /notes/1
   def destroy
     @note.destroy
-    render json: {messge: "Note deleted"}, status: 200
+    render json: {message: "Note deleted"}, status: 200
   end
 
   private
